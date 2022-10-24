5a9ed06e268b8ddf52f0d4916ec9cc872d798e4a
jerryzheng <zheng.jianli@outlook.com>
2022-10-24 09:46:47 +0800

    add temp

--- packages/magical-text-effect/index.js
+++ packages/magical-text-effect/index.js
@@ -8,9 +8,22 @@ const animate = (star) => {
   // not work
   // star.style.setProperty("--star-fill", colors[rand(0, 2)]);
 
+  // 重新播放动画
+  // 方法1 触发 reflow
   star.style.animation = "none";
   star.offsetHeight;
   star.style.animation = "";
+
+  // 方法2 延时设置
+  // star.style.animation = "none";
+  // setTimeout(() => {
+  //   star.style.animation = "";
+  // }, 0);
+
+  // 方法3 删除dom后重新插入
+  // const cloned = star.cloneNode(true);
+  // star.after(cloned);
+  // star.parentElement.removeChild(star);
 };
 
 let index = 0;
